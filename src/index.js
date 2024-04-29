bf6433a74eac661975d5dffda0c43f395639f399
BrvAlexis <brevieralexis@gmail.com>
2024-04-29 08:54:11 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import 'jquery';
+import '@popperjs/core';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style/index.scss'; // Attention ici, il faut bien mettre l'extension `.scss`
+
+
